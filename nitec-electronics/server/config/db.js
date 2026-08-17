c591a7f6558269c20645edb2b1316e8b3b6df483
Ankit Wosti <ankitwosti2@gmail.com>
2026-08-17 04:10:13 +0000

    database

---
+++ nitec-electronics/server/config/db.js
@@ -0,0 +1,12 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected : ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB not connected : ${error.message}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
