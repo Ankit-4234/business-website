6a795ef624b8ff4a0023aa12d228a0a135931ee6
Ankit Wosti <ankitwosti2@gmail.com>
2026-08-17 04:12:05 +0000

    Response

---
+++ nitec-electronics/server/utils/apiResponse.js
@@ -0,0 +1,17 @@
+export const successResponse = (
+  res,
+  statusCode,
+  message,
+  data = null,
+  extra = {},
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+    ...extra,
+  });
+};
+export const errorResponse = (res, statusCode, message) => {
+  return res.status(statusCode);
+};
