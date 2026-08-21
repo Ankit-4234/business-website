c7696a7ebb8a78b2a5752415f2f2b3f878c4024e
Ankit Wosti <ankitwosti2@gmail.com>
2026-08-21 07:44:59 +0000

    reviews

--- nitec-electronics/server/models/Review.js
+++ nitec-electronics/server/models/Review.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 const reviewSchema = new mongoose.Schema(
-    {
-        user: {type:mongoose.Schema.Types.ObjectId,ref="User",required:true},
-        product: {type:mongoose.Schema.Types.ObjectId, ref="Profuct",required:true},
-        rating: {type:Number,required:true,min:1, max:5},
-        comment: {type:String,required:true},
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Profuct",
+      required: true,
     },
-    {timestamps:true}
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true },
 );
-const Review = mongoose.model("Review",reviewSchema)
+const Review = mongoose.model("Review", reviewSchema);
 export default Review;
