079fa9710e0d69cabd175aaea5dc570af2fda230
Ankit Wosti <ankitwosti2@gmail.com>
2026-08-22 06:24:43 +0000

    phase one debug

---
+++ nitec-electronics/server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import authRoutes from "./routes/authRoutes.js";
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Nitec Electronics API is running"));
+
+app.use("/api/auth", authRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
